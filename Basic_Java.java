bdf19aba070e21dcae4d71da6b6ab7575d82414f
mariahelloworld <maria.elkhattabij1@gmail.com>
2023-09-03 15:42:55 -0400

    Pushing code for the first time

---
+++ Basic_Java.java
@@ -0,0 +1,33 @@
+package com.java.Sep_3_2023_Day2;
+
+public class Basic_Java {
+	
+	//Que1: how to create a Java Class?
+	//You will create a package under your src/main/java
+	//Right click on the package and create a new Class
+	//Make sure you select the main method
+	
+	//Que2:A basic Java Class- what does it look like
+	//Ans2: 1st line will have the package followed by a semi-colon
+	//then there will be public class NameOfTheClass
+	//Followed by a curly braces open{and it will be closing}
+	//{Body of the Class}
+	//{ }-> this represents a block.
+	//Important note-> Semi-colon does not apply on a block
+	//When we are writing only java it is advisable to use main method
+	
+
+	public static void main(String[] args) {
+		System.out.println(100 + 200); //syso + (Cntrl+Spacebar) + enter
+	}
+		
+		//Que3:What are keywords in java?
+		//Ans3:there are 50 reserved keywords in java represented as dark red Eclipse
+		
+		
+		//Que4:What are access modifiers?
+		//Ans4: Java has 4 access modifiers - public, private, protected and default
+		
+		//Keyword
+	}
+}
---
+++ What_is_Java.java
@@ -0,0 +1,14 @@
+package com.java.Sep_3_2023_Day2;
+
+public class What_is_Java {
+	
+	//Java is a High Level programming language
+	//Java is a Platform - because its has its own routine environment that is JVM (Java Virtual Machine)
+	                       // jvm provides a platform which accepts the byte code and executes on the machine
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
